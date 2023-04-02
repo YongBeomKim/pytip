9ef3c1288e64d51a125c95d7a6a4f23d37462144
buffet <saltman21@naver.com>
2023-04-02 16:33:39 +0900

    pytip 0.0.2 version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# pytip
-useful tool and tips of python
+# Python Useful Tip & Tools
+
+[![License](http://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat)](LICENSE)
+[![Docs](https://img.shields.io/badge/docs-stable-blue.svg)](https://domschl.github.io/ml-indie-tools/index.html)
+[![PyPI version fury.io](https://badge.fury.io/py/ml-indie-tools.svg)](https://pypi.python.org/pypi/ml-indie-tools/)
+
+
+# Installation
+
+Based on : https://github.com/YongBeomKim/pytip
+
+```bash
+	$ pip install pytip
+	$ pip install -i https://pypi.python.org/pytip pytip
+```
+
+© 2023 GitHub : https://github.com/YongBeomKim
---
+++ setup.py
@@ -0,0 +1,24 @@
+# find_packages
+# https://setuptools.pypa.io/en/latest/setuptools.html#using-find-packages
+# https://stackoverflow.com/questions/43253701/python-packaging-subdirectories-not-installed
+from setuptools import setup, find_packages
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pytip',
+    version='0.0.2',
+    description='useful tool and tips of python',
+    author='momukji lab',
+    author_email='saltman21@naver.com',
+    url='https://github.com/YongBeomKim/pytip',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'termcolor',
+        'requests',
+        'pandas',
+        'numpy',
+        'tqdm',
+    ],
+)
